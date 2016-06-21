0b91d53697b7484f1f7de644703b7fb69e583809
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-21 13:30:01 -0500

    Added Carthage support

---
+++ MTDates/MTDates.h
@@ -0,0 +1,18 @@
+//
+//  MTDates.h
+//  MTDates
+//
+//  Created by Alsey Coleman Miller on 6/21/16.
+//  Copyright © 2016 Mysterious Trousers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MTDates.
+FOUNDATION_EXPORT double MTDatesVersionNumber;
+
+//! Project version string for MTDates.
+FOUNDATION_EXPORT const unsigned char MTDatesVersionString[];
+
+#import <MTDates/NSDate+MTDates.h>
+#import <MTDates/NSDateComponents+MTDates.h>
